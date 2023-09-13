4dfd2c0c2c5a4003ae38f38c8f09a4c3ef5e0f86
smich254 <smich254@gmail.com>
2023-09-13 07:58:34 -0500

    Agregado funcionalidad get-update-usuario-no-admin

--- controllers/protectedController.go
+++ controllers/protectedController.go
@@ -1,36 +1,90 @@
 package controllers
 
 import (
+	"log"
+
 	"github.com/gofiber/fiber/v2"
+	"github.com/golang-jwt/jwt/v5"
+	"github.com/smich254/panaderia-api-rest-fiber/database"
 	// Importa cualquier otro paquete necesario
 )
 
 // GetUserProfile obtiene el perfil del usuario autenticado
 func GetUserProfile(c *fiber.Ctx) error {
-	// Implementar la lógica para obtener el perfil del usuario
-	return c.JSON(fiber.Map{"message": "User profile"})
+	log.Println("Fetching user profile...")
+
+	// Middleware JWT ya debería haber almacenado el token en c.Locals("user")
+	token, ok := c.Locals("user").(*jwt.Token)
+	if !ok {
+		log.Println("Unauthorized due to missing token")
+		return c.Status(fiber.StatusUnauthorized).JSON(fiber.Map{"error": "Not authorized"})
+	}
+
+	// Obtener el ID del usuario desde las reclamaciones del token JWT
+	claims := token.Claims.(jwt.MapClaims)
+	userId, ok := claims["userId"].(int)
+	if !ok {
+		log.Println("Unauthorized due to missing userId in claims")
+		return c.Status(fiber.StatusUnauthorized).JSON(fiber.Map{"error": "Not authorized"})
+	}
+
+	db := database.InitDB()
+	defer db.Close()
+
+	row := db.QueryRow("SELECT * FROM users WHERE id = ?", userId)
+	var user User
+	err := row.Scan(&user.ID, &user.Name, &user.LastName, &user.Password, &user.IsAdmin)
+	if err != nil {
+		log.Println("Error al escanear el perfil del usuario:", err)
+		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{"error": "Internal Server Error"})
+	}
+
+	// Omitir el campo de contraseña al devolver el perfil del usuario
+	user.Password = ""
+
+	return c.JSON(user)
 }
 
 // UpdateUserProfile actualiza el perfil del usuario autenticado
 func UpdateUserProfile(c *fiber.Ctx) error {
-	// Implementar la lógica para actualizar el perfil del usuario
+	log.Println("Updating user profile...")
+
+	// Middleware JWT ya debería haber almacenado el token en c.Locals("user")
+	token, ok := c.Locals("user").(*jwt.Token)
+	if !ok {
+		log.Println("Unauthorized due to missing token")
+		return c.Status(fiber.StatusUnauthorized).JSON(fiber.Map{"error": "Not authorized"})
+	}
+
+	// Obtener el ID del usuario desde las reclamaciones del token JWT
+	claims := token.Claims.(jwt.MapClaims)
+	userId, ok := claims["userId"].(int)
+	if !ok {
+		log.Println("Unauthorized due to missing userId in claims")
+		return c.Status(fiber.StatusUnauthorized).JSON(fiber.Map{"error": "Not authorized"})
+	}
+
+	db := database.InitDB()
+	defer db.Close()
+
+	user := new(User)
+	if err := c.BodyParser(user); err != nil {
+		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{"error": "Cannot parse JSON"})
+	}
+
+	// Verificar si el usuario intenta actualizar su propio perfil
+	if user.ID != userId {
+		log.Println("Unauthorized update of another user's profile")
+		return c.Status(fiber.StatusUnauthorized).JSON(fiber.Map{"error": "Not authorized"})
+	}
+
+	// Realizar la actualización del perfil del usuario
+	_, err := db.Exec("UPDATE users SET name = ?, lastName = ?, email = ?, password = ? WHERE id = ?",
+		user.Name, user.LastName, user.Email, user.Password, userId)
+	if err != nil {
+		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{"error": "Could not update user profile"})
+	}
+
 	return c.JSON(fiber.Map{"message": "User profile updated"})
 }
 
-// ListUsers lista todos los usuarios (solo para administradores)
-func ListUsers(c *fiber.Ctx) error {
-	// Implementar la lógica para listar todos los usuarios
-	return c.JSON(fiber.Map{"message": "List of users"})
-}
-
-// UpdateUser actualiza un usuario específico (solo para administradores)
-func UpdateUser(c *fiber.Ctx) error {
-	// Implementar la lógica para actualizar un usuario específico
-	return c.JSON(fiber.Map{"message": "User updated"})
-}
-
-// DeleteUser elimina un usuario específico (solo para administradores)
-func DeleteUser(c *fiber.Ctx) error {
-	// Implementar la lógica para eliminar un usuario específico
-	return c.JSON(fiber.Map{"message": "User deleted"})
-}
